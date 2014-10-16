851307c8065e390493d25a97c1cc8e401e30cf53
johnsont91 <Cruinex@Johnsons-MacBook-Pro.local>
2014-10-16 14:10:45 -0400

    com
    com

--- js/main.js
+++ js/main.js
@@ -19,22 +19,28 @@ $(function() {
 $(function() {
     WebFont.load({
         custom: {
-            families: ['Yanone Kaffeesatz'], // font-family name
+            families: ['acta-display'], // font-family name
             urls : ['css/main.css'] // URL to css
         },
         active: function() {
-            $('#bigtext').bigtext();
+            $('#attraction-headline').bigtext();
+            $('#banner-headline').bigtext();
+            $('#restaurant-headline').bigtext();
         }
     });
 });
 
-
  $(document).ready(function(){
+    $(".fitvids").fitVids(); // target your .container, .wrapper, .post, etc.
+}); 
+
+
+/* $(document).ready(function(){
     $(".toggle-button").next("ul").hide();
     $(".toggle-button").click(function(){
         $(".toggle-button").next("ul").slideToggle();
     });
-});
+}); */
 
 
  $(function () {
@@ -46,3 +52,18 @@ $(function() {
         namespace: "callbacks"
     });
 }); 
+
+$('#scene').parallax({
+  calibrateX: false,
+  calibrateY: true,
+  invertX: false,
+  invertY: true,
+  limitX: false,
+  limitY: 10,
+  scalarX: 2,
+  scalarY: 8,
+  frictionX: 0.2,
+  frictionY: 0.8,
+  originX: 0.0,
+  originY: 1.0
+});
--- js/plugins.js
+++ js/plugins.js
@@ -23,5 +23,7 @@
 
 // Place any jQuery/helper plugins below this line.
 
- //@prepros-append vendor/webfont.js 
- //@prepros-append vendor/bigtext.js 
+// @prepros-append vendor/webfont.js 
+// @prepros-append vendor/fitvids.js 
+// @prepros-append vendor/bigtext.js 
+// @prepros-append vendor/responsiveslides.js 
